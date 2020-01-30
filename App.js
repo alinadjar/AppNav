804614127078efa6614966c52aa3d82dd3c134b8
Ali Nadjar <ali.nadjar95@gmail.com>
2020-01-30 16:06:31 +0330

    Auth Flow Added

--- App.js
+++ App.js
@@ -1,189 +1,227 @@
 import React, { Component } from 'react';
-//import { View, Text, StyleSheet, Image } from 'react-native';
-import { Image } from 'react-native';
-import { NavigationActions, createAppContainer } from 'react-navigation';
-
-
-import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import {
-  Container, Header, Title, Content, Footer, FooterTab,
-  Button, Left, Right, Body, Icon, Text, Accordion
-} from 'native-base';
+  Text, View,
+  TextInput, ActivityIndicator, StatusBar, Button,
+  Modal, TouchableHighlight, AsyncStorage, StyleSheet, Alert
+} from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+//------------------------- Components
+class AuthLoadingScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> This is the Splash Screen ... for 3 Seconds!</Text>
+        <ActivityIndicator size="large" color='red' />
+        <StatusBar barStyle="default" barStyle="light-content" backgroundColor="#F99" />
+
+      </View>
+    );
+  }
+
+
+  componentDidMount() {
+    this._bootstrapAsync();
+  }
+
+
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+
+    setTimeout(() => {
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+
+    }, 3000);
+  }
 
 
 
-//-----------------------------------------------------------------------
-const dataArray = [
-  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
-];
+}
 
-
-
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-class MyHomeScreen extends Component {
-
+class HomeScreen extends Component {
   static navigationOptions = {
-    drawerLabel: 'Sup Lead',
-    drawerIcon: () => (
-      <Image
-        style={{ width: 30, height: 30 }}
-        sourec={require('./src/assets/images/benz.png')}
-      />
-    )
-  }
-
-  navigateToScreen = (route) => () => {
-    const navigationAction = NavigationActions.navigate({
-      routeName: route
-    });
-    this.props.navigation.dispatch(navigationAction);
-  }
+    title: 'Welcome to the app!',
+  };
 
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-          <Text>Home Screen ....</Text>
-          <Text onPress={this.navigateToScreen('Notifications')}>Click Here </Text>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <StatusBar barStyle="default" barStyle="light-content" backgroundColor="#F99" />
+        <Text>Welcome to Home....Landing Page</Text>
+        <Button title="Show me more of the app" onPress={this._showMoreApp} />
+        <Button style={{ marginTop: 7 }} title="sign me out :)" onPress={this._signOutAsync} />
+      </View>
+    );
+  }
+
+  _showMoreApp = () => {
+    this.props.navigation.navigate('Other');
+  };
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  };
+
+}
+
+class OtherScreen extends Component {
+
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
+
+  render() {
+    return (
+      <View style={{ marginTop: 22 }}>
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            this.setModalVisible(!this.state.modalVisible);
+          }}
+        >
+          <View style={{ backgroundColor: 'rgba(0,0,0,0.5)', flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+            <View style={{
+              backgroundColor: '#FEE', width: '85%', borderTopLeftRadius: 10,
+              borderTopRightRadius: 10, overflow: 'hidden'
+            }}>
+
+              <View style={{ backgroundColor: '#00F', height: 25, alignItems: 'center' }}><Text style={{ color:'#FFF', fontSize: 16}}>Header</Text></View>
+              <View style={{ height: 160, padding: 5}}>
+                <Text>Hello Modal! :)</Text>
+
+                <Text style={{ fontSize: 24 }}>some text as the filling ...</Text>
+                <Icon  name='ios-checkmark-circle' size={60} color='#2F1' style={{ marginLeft: '45%', marginTop: 8}}/>
+              </View>
+              <View style={{ backgroundColor: '#00F', height: 40, justifyContent: 'center', alignItems: 'center', paddingTop: 5, paddingBottom: 5 }}>
+                <TouchableHighlight style={{ backgroundColor: '#64A239' , borderRadius:5}}
+                  onPress={() => {
+                    this.setModalVisible(!this.state.modalVisible);
+                  }}>
+                  <Text style={{ color: '#FFF', fontSize: 22, padding: 5 }}>Hide Modal</Text>
+                </TouchableHighlight>
+              </View>
+
+
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+
+      </View>
     );
   }
 }
 
+class SignInScreen extends Component {
 
-class MyAboutScreen extends Component {
-
-  static navigationOptions = {
-    drawerLabel: 'About',
-    drawerIcon: () => <Ionicons name="ios-brush" size={24} />
+  state = {
+    username: '',
+    password: ''
   }
 
-  navigateToScreen = (route) => () => {
-    const navigationAction = NavigationActions.navigate({
-      routeName: route
-    });
-    this.props.navigation.dispatch(navigationAction);
+  onChangeText = (field, text) => {
+    //alert(field);
+    this.setState(state => ({
+      ...state,
+      [field]: text
+    }));
   }
 
   render() {
     return (
-      // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      //   <Text>This is the about screen....</Text>
-      //   {/* <Button
-      //     onPress={() => this.props.navigation.navigate('Home')}
-      //     title="Go to Home"
-      //   /> */}
-      //   <Text onPress={this.navigateToScreen('Home')}>Click Here </Text>
-      // </View>
-
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-          <Text>About Screen ....</Text>
-          <Accordion dataArray={dataArray} expanded={0} style={{ width: '100%' }}
-            headerStyle={{ backgroundColor: "#b7daf8" }}
-            contentStyle={{ backgroundColor: '#F55' }}
-            icon="add"
-            expandedIcon="remove"
-            iconStyle={{ color: "green" }}
-            expandedIconStyle={{ color: "red" }}
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', borderColor: '#F00', border: 3 }}>
+        <Text> SignIn Page </Text>
+        <View style={{ backgroundColor: '#FFF', width: '75%' }} >
+          <TextInput
+            type='username'
+            placeholder='username'
+            style={styles.textInp}
+            onChangeText={text => this.onChangeText('username', text)}
+            value={this.state.username}
           />
-          <Text onPress={this.navigateToScreen('Notifications')}>Click Here </Text>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+        </View>
+        <View style={{ backgroundColor: '#FFF', width: '75%', marginTop: 4 }} >
+          <TextInput
+            type='password'
+            secureTextEntry={true}
+            placeholder='password'
+            style={styles.textInp}
+            onChangeText={text => this.onChangeText('password', text)}
+            value={this.state.password}
+          />
+        </View>
+
+        {/* <Button title="Sign in!" onPress={this._signInAsync} /> */}
+        <TouchableOpacity
+          onPress={this._signInAsync}
+          style={styles.submitBtn}
+        >
+          <Text style={{ color: '#FFF' }}>Sign in!</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
+
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem('token', 'abc');
+    this.props.navigation.navigate('App');
+  }
+
 }
 
 
+const styles = StyleSheet.create({
+  textInp: { height: 40, borderColor: 'gray', borderWidth: 1, fontSize: 21, textAlign: 'center' },
+  submitBtn: {
+    backgroundColor: '#29F',
+    color: '#F00',
+    fontWeight: 'bold',
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 4,
+  }
+});
 
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
-
-const customNavigator = (props) => (
-  <Container>
-    <Header style={{ height: 30 }} >
-      <Body>
-        <Image
-          style={{ width: 20, height: 25 }}
-          source={require('./src/assets/images/setting.png')} />
-      </Body>
-    </Header>
-    <Content>
-      <DrawerItems {...props} />
-    </Content>
-  </Container>
-);
-
-
-
-
-const MyNavigator = createAppContainer(createDrawerNavigator({
-  Home: {
-    screen: MyHomeScreen,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Supreme Leader'
-    })
-  },
-  Notifications: MyAboutScreen
-},
+const switchNavig = createSwitchNavigator(
   {
-    initialRouteName: 'Home',
-    //drawerWidth: 200,
-    //drawerPosition: 'right',
-    //drawerBackgroundColor: 'red',
-    //drawerType: 'front', // back , slide --> push the component as the drawer opens? or just open on that?!
-    contentComponent: customNavigator,
-    contentOptions: {// this config will apply to the items inside the drawer (drawerItems)
-      activeTintColor: 'red',
-      activeBackgroundColor: 'blue',
-    },
-  })
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
 );
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <MyNavigator />
-    );
-  }
-}
+export default createAppContainer(switchNavig);
+
+
