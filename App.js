ccbf3942cc2112dd32c28dd73fa88cd26543db12
Ali Nadjar <ali.nadjar95@gmail.com>
2020-02-04 09:41:09 +0330

    temp

--- App.js
+++ App.js
@@ -10,11 +10,21 @@ import SplashScreen from './src/simpleChatApp/Splash/splash1';
 const InitialNavigator = createSwitchNavigator({
   Splash: SplashScreen,
   Landing: HomeScreenIndex,
+},
+{
+  // `initialRouteName` tells the React Navigation navigator
+  // which route to start at. If you don't specify an initial
+  // route it'll choose the first route in the navigator config.
+  // In this example we tell it to start on the 'Splash' route,
+  // otherwise it would have shown the 'App' route.
+  initialRouteName: 'Splash'
 });
+
+//export default createAppContainer(InitialNavigator);
+
 
 
 const FinalApp = createAppContainer(InitialNavigator);
-
 export default class AwesomeApp extends Component {
   render() {
     return (
--- src/simpleChatApp/Splash/splash1.js
+++ src/simpleChatApp/Splash/splash1.js
@@ -8,32 +8,45 @@ export default class SplashScreen extends Component {
 
 
     performTimeConsumingTask = async () => {
-      return new Promise((resolve) =>
-        setTimeout(
-          () => { resolve('result') },
-          2000
+        return new Promise((resolve) =>
+            setTimeout(
+                () => { resolve('result') },
+                2000
+            )
         )
-      )
     }
-  
+
     async componentDidMount() {
-      // Preload data from an external API
-      // Preload data using AsyncStorage
-      const data = await this.performTimeConsumingTask();
-  
-      if (data !== null) {
-        this.props.navigation.navigate('Landing');
-      }
+        // Preload data from an external API
+        // Preload data using AsyncStorage
+        const data = await this.performTimeConsumingTask();
+
+        if (data !== null) {
+            this.props.navigation.navigate('Landing');
+        }
     }
-  
+
+
+    // componentDidMount() {
+    //     // When mounted, wait one second, then navigate to App
+    //     setTimeout(() => {
+    //         // Components that are placed inside a React Navigation
+    //         // navigator will receive the `navigation` prop.
+    //         // It's main usage is to trigger navigation events.
+    //         // Right here we're telling it to navigate to the route
+    //         // with the name 'App'.
+    //         this.props.navigation.navigate('App');
+    //     }, 5000);
+    // }
+
     render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'orange' }}>
-          <Text> LOADING SPLASH </Text>
-          <Text> ... </Text>
-          <Text> 3 Seconds </Text>
-          <ActivityIndicator size="large" />
-        </View>
-      );
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'orange' }}>
+                <Text> LOADING SPLASH </Text>
+                <Text> ... </Text>
+                <Text> 3 Seconds </Text>
+                <ActivityIndicator size="large" />
+            </View>
+        );
     }
-  }
+}
