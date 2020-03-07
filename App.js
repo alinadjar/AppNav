2e562fd7f88fa17afff579dad92468d4a8b70432
Ali Nadjar <ali.nadjar95@gmail.com>
2020-03-07 14:17:25 +0330

    basic file/apk download progress success

--- App.js
+++ App.js
@@ -1,98 +1,365 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
   Text,
   View,
-  StyleSheet,
-  Alert,
   TouchableOpacity,
-  Image
+  Alert,
+  PermissionsAndroid,
+  Image, Button,
+  Platform, ToastAndroid, ProgressBarAndroid
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import RNFetchBlob from 'rn-fetch-blob';
 
+import { AlertModal } from './src/utils/modals/AlertModal';
+
+
+
+
+export async function request_storage_runtime_permission() {
+
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      {
+        'title': 'ReactNativeCode Storage Permission :)  Ha...Ha..HA..',
+        'message': 'ReactNativeCode App needs access to your storage to download Photos.'
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+
+      Alert.alert("Storage Permission Granted.");
+    }
+    else {
+
+      Alert.alert("Storage Permission Not Granted");
+
+    }
+  } catch (err) {
+    console.warn(err)
+  }
+}
 
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      torchOn: false
+  state = {
+    progress: 0,
+    loading: false,
+    modal_Alert_Show: false,
+    modalBodyText: '-'
+  }
+
+  async componentDidMount() {
+    //await request_storage_runtime_permission()
+  }
+
+
+  downloadImage = () => {
+    var date = new Date();
+    var image_URL = 'http://tigmis.tabarok.ir/sites/all/themes/adp/images/people/Ali_Nadjar.jpg';
+    var ext = this.getExtention(image_URL);
+    ext = "." + ext[0];
+    const { config, fs } = RNFetchBlob;
+    let PictureDir = fs.dirs.PictureDir;
+
+    console.log(PictureDir);
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        useDownloadManager: true,
+        notification: true,
+        path: PictureDir + "/image_" + Math.floor(date.getTime() + date.getSeconds() / 2) + ext,
+        description: 'Image'
+      }
+    }
+    config(options).fetch('GET', image_URL).then((res) => {
+      console.log(res);
+      Alert.alert("Image Downloaded Successfully.");
+    });
+  }
+
+
+  getExtention = (filename) => {
+    return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) :
+      undefined;
+  }
+
+
+
+  downloadAPK = () => {
+
+    const android = RNFetchBlob.android;
+    const downloadUrl = "http://192.168.87.61:7477/TJS.apk"; //"https://app.733.ir/download/asanpardakht.apk";
+    const { config, fs } = RNFetchBlob;
+
+    console.log(`now inside downloadAPK, from ${downloadUrl}`);
+
+
+    this.setState({
+      modal_Alert_Show: true,
+      progress: 0,
+      loading: true
+    });
+
+
+    //RNFetchBlob.config({
+    config({
+      // appendExt: 'apk',
+      // timeout: 180000,
+      // addAndroidDownloads: {
+      //   notification: true,
+      //   useDownloadManager: true,
+      //   mime: 'application/vnd.android.package-archive',
+      //   mediaScannable: true,
+      //   title: "Ha..Ha..Ha..",
+      //   description: ';)',
+      //   path: fs.dirs.PictureDir
+      // }
+      path: fs.dirs.DownloadDir + "/A.apk",
+      fileCache: true
+    })
+      .fetch("GET", downloadUrl)
+      .progress({ interval: 500 }, (received, total) => {
+        console.log('progress', received / total);
+        console.log('========> ' + (received / total) * 100);
+        //alert((received / total) * 100);
+
+
+        this.setState({
+          progress: Math.round((received / total) * 10000) / 100,
+          modalBodyText: this.state.modalBodyText + '*'
+        })
+      })
+      .then(res => {
+        console.log(res);
+        this.setState({
+          modal_Alert_Show: false,
+          progress: 100,
+          loading: false
+        });
+
+        ToastAndroid.showWithGravity(
+          "Your file has been downloaded to downloads folder!",
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM
+        );
+
+        if (res.respInfo.timeout) {
+          Linking.openURL(downloadUrl)
+          return;
+        }
+        android.actionViewIntent(res.path(), 'application/vnd.android.package-archive')
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ modal_Alert_Show: false });
+        alert(error);
+        Linking.openURL(downloadUrl)
+      });
+  }
+
+
+  async downloadFile() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: "Storage Permission",
+          message: "App needs access to memory to download the file "
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        this.downloadAPK();
+      } else {
+        Alert.alert(
+          "Permission Denied!",
+          "You need to give storage permission to download the file"
+        );
+      }
+    } catch (err) {
+      console.warn(err);
     }
   }
 
-  onBarCodeRead = (e) => { Alert.alert("Barcode value is" + e.data, "Barcode type is" + e.type); }
 
-  takePicture = async () => {
-    try {
-      const data = await this.camera.takePictureAsync();
-      console.log('Path to image: ' + data.uri);
-    } catch (err) {
-      // console.log('err: ', err);
-      console.log('Error insode catch: '+ err);
-    }
-  };
+  downloadAPK2 = () => {
+    const android = RNFetchBlob.android;
+    const downloadUrl = "https://app.733.ir/download/asanpardakht.apk";//"http://192.168.87.61:7477/TJS.apk"; 
+    const { config, fs } = RNFetchBlob;
+
+    let dirs = RNFetchBlob.fs.dirs;
+    const filePath = `${dirs.DocumentDir}`;
+    const fileName = 'test';
+
+    console.log(`now inside downloadAPK2, from ${downloadUrl}`);
+
+    this.setState({ modal_Alert_Show: true });
+
+    //RNFetchBlob.config({
+    config({
+      appendExt: 'apk',
+      timeout: 120000,
+      path: `${filePath}/${fileName}`,
+      fileCache: true,
+      addAndroidDownloads: {
+        notification: true,
+        useDownloadManager: true,
+        mime: 'application/vnd.android.package-archive',
+        mediaScannable: true,
+        title: "Ha..88..Ha..",
+        description: ';)',
+        path: fs.dirs.PictureDir
+      }
+    })
+      .fetch("GET", downloadUrl)
+      // listen to download progress event, every 10%
+      // .progress({ count: 10 }, (received, total) => {
+      //   console.log('progress', received / total)
+      // })
+      .progress({ interval: 500 }, (received, total) => {
+        console.log('progress', received / total);
+        console.log('========> ' + (received / total) * 100);
+        //alert((received / total) * 100);
+        this.setState({
+          progress: (received / total) * 100,
+          modalBodyText: this.state.modalBodyText + '*'
+        })
+      })
+      .then(res => {
+
+        this.setState({
+          progress: 0,
+          //modal_Alert_Show: true
+        });
+
+
+        this.setState({ modal_Alert_Show: false });
+
+        // RNFetchBlob.fs.exists(res.path()).then(exist=>{
+        //     console.log(`file ${exist ? '' : 'not'} exists`)
+        // }).catch(
+        //     err=>console.log('error while checking existance',err)
+        // );
+        if (Platform.OS === 'ios') {
+          RNFetchBlob.ios.openDocument(res.path());
+        } else {
+          // RNFetchBlob.android.actionViewIntent(res.path(), "application/pdf");
+          android.actionViewIntent(res.path(), 'application/vnd.android.package-archive');
+        }
+
+
+        // console.log(res);
+
+        // if (res.respInfo.timeout) {
+        //   Linking.openURL(downloadUrl)
+        //   return;
+        // }
+        // android.actionViewIntent(res.path(), 'application/vnd.android.package-archive');
+      })
+      .catch((errorMessage, statusCode) => {
+        console.log("error with downloading file", errorMessage);
+        console.log(error);
+        this.setState({ modal_Alert_Show: false });
+        //Linking.openURL(downloadUrl)
+      });
+  }
+
+
+
+
+  close_AlertModal_callback_btnReturn = () => {
+    this.setState({ modal_Alert_Show: false });
+  }
+
+
+  close_AlertModal_callback = () => {
+    this.setState({ modal_Alert_Show: false });
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <RNCamera
-          style={styles.preview}
-          //torchMode={this.state.torchOn ? Camera.constants.TorchMode.on : Camera.constants.TorchMode.off}
-          onBarCodeRead={this.onBarCodeRead}
-          ref={cam => this.camera = cam}
-        //aspect={Camera.constants.Aspect.fill}
-        >
-          <Text style={{
-            backgroundColor: 'white'
-          }}>BARCODE SCANNER</Text>
-          <View style={styles.captureContainer}>
-            <TouchableOpacity style={styles.capture} onPress={this.takePicture}>
-              {/* <Icon style={styles.iconCamera}>camera</Icon> */}
-              <Text>Take Photo</Text>
-            </TouchableOpacity>
+      <>
+        <View style={styles.MainContainer}>
+
+          <View style={{ borderWidth: 1 }}>
+            <Image source={{ uri: 'http://tigmis.tabarok.ir/sites/all/themes/adp/images/people/Ali_Nadjar.jpg' }}
+              style={{ width: 200, height: 300 }} />
           </View>
-        </RNCamera>
-        <View style={styles.bottomOverlay}>
-          <TouchableOpacity onPress={() => this.handleTourch(this.state.torchOn)}>
-            <Image style={styles.cameraIcon} source={this.state.torchOn === true ? require('./src/images/flasher_on.png') : require('./src/images/flasher_off.png')} />
+
+          <TouchableOpacity style={styles.button} onPress={this.downloadImage} >
+            <Text style={styles.text}>Click To Download Above Image</Text>
           </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button, { backgroundColor: '#F55', }]} onPress={this.downloadAPK} >
+            <Text style={styles.text}>Click To Download APK</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button, { backgroundColor: '#F55', }]} onPress={this.downloadAPK2} >
+            <Text style={styles.text}>Download APK with Progress</Text>
+          </TouchableOpacity>
+
+
+          <Button onPress={() => this.downloadFile()} title="Download" />
+
+
+          {this.state.loading ? (
+            <View>
+              <ProgressBarAndroid
+                styleAttr="Large"
+                indeterminate={false}
+                progress={this.state.progress}
+              />
+              <Text>{this.state.progress}</Text>
+            </View>
+
+          ) : null}
+
+
         </View>
-      </View>)
+
+        <AlertModal
+          modalVisible={this.state.modal_Alert_Show}
+          returnTXT='OK'
+          bodyTXT={this.state.modalBodyText}
+          logoType='CONFIRM'
+          logoTXT='SUCCESS'
+          btnReturnCallback={this.close_AlertModal_callback_btnReturn}
+          closeCallback={this.close_AlertModal_callback}
+        />
+
+      </>
+    );
   }
 
-  handleTourch = (value) => {
-    if (value === true) {
-      this.setState({ torchOn: false });
-    } else {
-      this.setState({ torchOn: true });
-    }
-  }
+
+
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  MainContainer: {
     flex: 1,
-    flexDirection: 'row',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
-    borderWidth:1,
-    borderColor: '#F00'
+    backgroundColor: '#F5FCFF',
   },
-  cameraIcon: {
-    margin: 5,
-    height: 40,
-    width: 40
+
+  button: {
+
+    width: '80%',
+    paddingTop: 3,
+    paddingBottom: 3,
+    backgroundColor: '#2E7D32',
+    borderRadius: 7,
+    margin: 10
   },
-  bottomOverlay: {
-    position: "absolute",
-    width: "100%",
-    flex: 20,
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 5
+  }
 });
 
 
